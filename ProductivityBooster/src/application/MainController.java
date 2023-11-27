5347a2308cd02111656ea3a6b82d612fe777ac7f
Vedant-Deshpande <114270105+Vedant-Deshpande@users.noreply.github.com>
2023-11-27 11:26:21 -0500

    Created Tomato Timer

--- ProductivityBooster/src/application/MainController.java
+++ ProductivityBooster/src/application/MainController.java
@@ -15,6 +15,9 @@ public class MainController {
 
     @FXML
     private Button btnAddTask;
+    
+    @FXML
+    private Button btnStartTimer;
 
     @FXML
     void addBtnClick(ActionEvent event) {
@@ -28,6 +31,30 @@ public class MainController {
             dialogStage.setTitle("New Task Dialog");
             dialogStage.initModality(Modality.WINDOW_MODAL);
             dialogStage.initOwner(btnAddTask.getScene().getWindow());
+
+            // Set the loaded content as the scene for the dialog stage
+            Scene scene = new Scene(root);
+            dialogStage.setScene(scene);
+
+            // Show the dialog and wait for it to be closed
+            dialogStage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @FXML
+    void addBtnClick1(ActionEvent event) {
+        try {
+            // Load the newTask.fxml file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("newTimer.fxml"));
+            Parent root = loader.load();
+
+            // Create a new stage for the dialog
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Tomato Timer Dialog Box");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(btnStartTimer.getScene().getWindow());
 
             // Set the loaded content as the scene for the dialog stage
             Scene scene = new Scene(root);
