24b75c87cf291e20b5c3b59cbbb94e57a6278062
Anuraag Bathula <bathula.anuraag.100@gmail.com>
2023-11-30 17:56:48 -0500

    Committing basic fxml manipulation
    Committing basic fxml manipulation. Add task still broken.

--- ProductivityBooster/src/application/MainController.java
+++ ProductivityBooster/src/application/MainController.java
@@ -1,15 +1,20 @@
 package application;
 
+
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class MainController {
 
@@ -39,4 +44,8 @@ public class MainController {
             e.printStackTrace();
         }
     }
+
+    
+    @FXML GridPane todoGridPane;
+
 }
