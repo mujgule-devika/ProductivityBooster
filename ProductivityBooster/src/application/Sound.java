8d8032f92881f0b4cc5bb708d35e26f44567e893
Vedant-Deshpande <114270105+Vedant-Deshpande@users.noreply.github.com>
2023-12-03 16:03:12 -0500

    Alarm sound and colour change in Tomato Timer

---
+++ ProductivityBooster/src/application/Sound.java
@@ -0,0 +1,27 @@
+package application;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Sound {
+	private AudioInputStream audioStream;
+    private Clip audioClip;
+
+    public Sound(String song) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+        File audioFile = new File(song);
+        audioStream = AudioSystem.getAudioInputStream(audioFile);
+
+        AudioFormat format = audioStream.getFormat();
+        DataLine.Info info = new DataLine.Info(Clip.class,format);
+        audioClip = (Clip) AudioSystem.getLine(info);
+    }
+    public void playAudio() throws IOException, LineUnavailableException {
+        audioClip.open(audioStream);
+        audioClip.start();
+        audioClip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+    public void stopAudio() throws IOException {
+        audioClip.close();
+        audioStream.close();
+    }
+}
--- ProductivityBooster/src/application/TomatoTimerController.java
+++ ProductivityBooster/src/application/TomatoTimerController.java
@@ -1,17 +1,17 @@
 package application;
 
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-
-import java.io.IOException;
-
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
 
 public class TomatoTimerController {
 
@@ -26,12 +26,13 @@ public class TomatoTimerController {
 
     @FXML
     private Button resetButton;
-    
+
     @FXML
     private AnchorPane timerPane;
 
     private int timeSeconds = 1500; // 25 minutes in seconds
     private Timeline timeline;
+    private Sound alarmSound;
 
     @FXML
     private void handleStartButton(ActionEvent event) {
@@ -43,6 +44,7 @@ public class TomatoTimerController {
     private void handleStopButton(ActionEvent event) {
         // Implement logic to stop the timer
         timeline.stop();
+        stopAlarmSound();
     }
 
     @FXML
@@ -50,19 +52,24 @@ public class TomatoTimerController {
         // Implement logic to reset the timer
         timeline.stop();
         timeSeconds = 1500; // Reset to 25 minutes
-        timerLabel.setText("25:00"); // Update the label
+        updateTimerLabel();
+        timerPane.setStyle("-fx-background-color: #FCCA46;"); // Reset background color
+        stopAlarmSound();
     }
-    
+
     @FXML
     private void initialize() {
         // You can perform any additional initialization here
         updateTimerLabel();  // Ensure the label is updated initially
+
+        // Load the alarm sound
+        try {
+            alarmSound = new Sound("/Users/vedant/Documents/GitHub/ProductivityBooster/ProductivityBooster/src/application/analog-watch-alarm_daniel-simion.wav");
+        } catch (IOException | UnsupportedAudioFileException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
     }
-    
-    public TomatoTimerController() {
-        // Call initialize method
-    }
-    
+
     private void startTimer() {
         timeline = new Timeline();
         timeline.setCycleCount(Timeline.INDEFINITE);
@@ -71,22 +78,51 @@ public class TomatoTimerController {
             timeSeconds--;
 
             // Update the timer label with the remaining time
-            timerLabel.setText(String.format("%02d:%02d", timeSeconds / 60, timeSeconds % 60));
+            updateTimerLabel();
 
             if (timeSeconds <= 0) {
                 // Timer has reached zero, implement logic for what happens next
-                // For example, switch to break time or reset the timer
                 timeline.stop();
+                handleTimerEnd();
             }
         });
 
         timeline.getKeyFrames().add(keyFrame);
         timeline.play();
-        updateTimerLabel();
     }
-    
+
     private void updateTimerLabel() {
         timerLabel.setText(String.format("%02d:%02d", timeSeconds / 60, timeSeconds % 60));
     }
-    
+
+    private void handleTimerEnd() {
+        // Play the alarm sound
+        playAlarmSound();
+
+        // Change the background color to red
+        timerPane.setStyle("-fx-background-color: #FF0000;");
+
+        // You can add more logic here based on what should happen when the timer reaches 00:00
+    }
+
+    private void playAlarmSound() {
+        if (alarmSound != null) {
+            try {
+                alarmSound.playAudio();
+            } catch (IOException | LineUnavailableException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void stopAlarmSound() {
+        if (alarmSound != null) {
+            try {
+                alarmSound.stopAudio();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
